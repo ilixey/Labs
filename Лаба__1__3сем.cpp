2e15e87f9f23a3a497c8a0d1fd818c95ea5e9cd8
ilixey <71875708+ilixey@users.noreply.github.com>
2020-09-25 12:11:52 +0300

    Add files via upload

---
+++ Лаба__1__3сем.cpp
@@ -0,0 +1,25 @@
+﻿#include<iomanip>
+#include<iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+
+	double a, b, c, x;
+
+	cout << "Уравнение вида: a*x+b=c\nВведите коофиценты\n";
+
+	cout << "а=";
+	cin >> a;
+	cout << "\nb=";
+	cin >> b;
+	cout << "\nc=";
+	cin >> c;
+
+	cout << a << "*x + " << b << "=" << c;
+	x = (c - b) / a;
+
+	cout << "\nОтвет:\n";
+
+	cout << setw(10) << setfill('%') << setprecision(4) << x;
+}
